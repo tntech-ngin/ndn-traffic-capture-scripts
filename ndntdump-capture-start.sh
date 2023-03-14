d9e9d4985b652edebf3bc8cbc7f69afdd070ae3a
Sankalpa Timilsina <sankalpatimilsina12@gmail.com>
2023-03-14 01:01:04 +0000

    Use simple services with directives to trigger actions

--- ndntdump-capture-start.sh
+++ ndntdump-capture-start.sh
@@ -15,7 +15,9 @@ mkdir -p "$OUT_DIR"
 IFACE_LIST=$(ip -o link show | awk -F': ' '{print $2}')
 for IFACE in $IFACE_LIST; do
     IFACE=$(echo "$IFACE" | sed 's/[^a-zA-Z0-9_-]/-/g')
-    docker stop ndntdump-$IFACE >/dev/null 2>&1
+    if [[ $(docker ps -q -f name=ndntdump-$IFACE) ]]; then
+        docker stop ndntdump-$IFACE >/dev/null 2>&1
+    fi
     id=$(docker run --network host --name ndntdump-$IFACE -v "$OUT_DIR":/dump --rm -d ndntdump --ifname $IFACE -w "$OUT_DIR/output-$DATE-$IFACE.pcapng.zst")
     echo "Started ndntdump container for interface $IFACE with ID: $id"
 done
--- ndntdump-capture-stop.sh
+++ ndntdump-capture-stop.sh
@@ -3,5 +3,9 @@
 IFACE_LIST=$(ip -o link show | awk -F': ' '{print $2}')
 for IFACE in $IFACE_LIST; do
     IFACE=$(echo "$IFACE" | sed 's/[^a-zA-Z0-9_-]/-/g')
-    docker stop ndntdump-$IFACE >/dev/null 2>&1
+
+    if [[ $(docker ps -q -f name=ndntdump-$IFACE) ]]; then
+        docker stop ndntdump-$IFACE >/dev/null 2>&1
+    fi
 done
+sleep 15
