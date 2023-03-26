e9efe3b3bd8e0ea0bd75ebcafc08966844d13cd4
Sankalpa Timilsina <sankalpatimilsina12@gmail.com>
2023-03-26 02:24:24 +0000

    Capture at 18:00:00 CST everyday

--- uninstall.sh
+++ uninstall.sh
@@ -6,7 +6,8 @@ sudo systemctl stop "ndntdump*"
 # Remove services
 sudo rm /etc/systemd/system/ndntdump-capture-start.service
 sudo rm /etc/systemd/system/ndntdump-capture-stop.service
-sudo rm /etc/systemd/system/ndntdump-capture.timer
+sudo rm /etc/systemd/system/ndntdump-capture-start.timer
+sudo rm /etc/systemd/system/ndntdump-capture-stop.timer
 sudo rm /usr/local/bin/ndntdump-capture-start.sh
 sudo rm /usr/local/bin/ndntdump-capture-stop.sh
 
